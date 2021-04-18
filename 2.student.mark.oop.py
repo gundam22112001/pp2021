95d4d6f71fdf1e92600452d430874d6eeb3c8c37
gundam22112001 <gundam22112001@gmai.com>
2021-04-18 11:09:49 +0700

    Vũ Đức Chính BI10-024

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,97 +1,89 @@
 class Student:
-    def __init__(self, id = "", name = "", dob = ""):
+    def __init__(self, id="", name="", dob=""):
         self.__id = id
-        self.__name = name
+        self.name = name
         self.__dob = dob
-
 
     def getId(self):
         return self.__id
 
-
     def getName(self):
-        return self.__name
-
+        return self.name
 
     def getDob(self):
         return self.__dob
 
-
     def input(self):
         self.__id = input("Enter Student Id: ")
-        self.__name = input("Enter Student Name: ")
+        self.name = input("Enter Student Name: ")
         self.__dob = input("Enter Student Date of Birth: ")
 
-
     def __str__(self):
-        return "Student: "+self.__name+" with id of "+self.__id+" born in "+self.__dob
-
+        return "Student: " + self.name + " with id of " + self.__id + " born in " + self.__dob
 
     def describe(self):
         print(self.__str__())
 
 
 class Mark:
-    def __init__(self, student, course, mark=0):
-        self.__student = student
+    def __init__(self, studentName, course, mark=0):
+        self.__studentName = studentName
         self.__course = course
         self.__mark = mark
 
-
     def input(self):
-        #self.__student = input("Enter the Student's Name: ")
-        #self.__course = input("Enter the Course's Name: ")
-        print(f"Enter Student's mark for {self.__student.getName()}")
-        self.__mark = input(f"in {self.__course.getName()}: ")
-
+        print(f"Enter Student's mark for {self.__studentName}")
+        self.__mark = input(f"in {self.__course}: ")
 
     def getMark(self):
         return self.__mark
 
+    def getCourse(self):
+        return self.__course
+
+    def getName(self):
+        return self.__studentName
 
     def __str__(self):
-        return "Student "+self.__student.getName()+" has a mark of "+str(self.__mark)+" in "+self.__course.getName()
-
+        return "Student " + self.__studentName.getName() + " has a mark of " + str(
+            self.__mark) + " in " + self.__course
 
     def describe(self):
         print(self.__str__())
 
 
 class Course:
-    def __init__(self, id = "", name = ""):
+    def __init__(self, id="", name=""):
         self.__id = id
-        self.__name = name
-
+        self.name = name
 
     def getId(self):
         return self.__id
 
-
     def getName(self):
-        return self.__name
-
+        return self.name
 
     def input(self):
         self.__id = input("Enter Course Id: ")
-        self.__name = input("Enter Course Name: ")
-
+        self.name = input("Enter Course Name: ")
 
     def __str__(self):
-        return "Course: "+self.__name+" with id of "+self.__id
-
+        return "Course: " + self.name + " with id of " + self.__id
 
     def describe(self):
         print(self.__str__())
 
 
-
-NumberStd = int(input("Enter number of Students: "))
 ClassRoom = []
 ListOfCourse = []
+Marks = []
+
+NumberStd = int(input("Enter number of Students: "))
+
 for i in range(NumberStd):
     s = Student()
     s.input()
-    ClassRoom +=[s]
+    ClassRoom += [s]
 
 for student in ClassRoom:
     print(student)
@@ -101,33 +93,35 @@ NumberOfCourse = int(input("Enter number of Courses: "))
 for i in range(NumberOfCourse):
     c = Course()
     c.input()
-    ListOfCourse +=[c]
+    ListOfCourse += [c]
 
 for c in ListOfCourse:
     print(c)
 
-m = Mark(ClassRoom[0],ListOfCourse[0])
-m.input()
-m.describe()
 
-Marks = {
-    "name" : [],
-    "mark" : []
-}
-
-Course = input("Enter the course name: ")
-
-for i in range(NumberOfCourse):
-    if Course == ListOfCourse[i].getName():
-        for j in range(NumberStd):
-            m = Mark(ClassRoom[j], ListOfCourse[i])
-            Marks["name"].append(ClassRoom[j].getName())
-            m.input()
-            Marks["mark"].append(m.getMark())
+def choseCourse():
+    Course = input("Enter the course name: ")
+    return Course
 
 
-Course = input("Enter the course name: ")
+def inputMark(Course):
+    print("Inputing Mark now")
+    for i in range(NumberOfCourse):
+        if Course == ListOfCourse[i].getName():
+            for j in range(NumberStd):
+                m = Mark(ClassRoom[j].getName(), ListOfCourse[i].getName())
+                m.input()
+                Marks.append(m)
 
-for i in range(NumberOfCourse):
-    if Course == ListOfCourse[i].getName():
-        print(Marks)
+
+def printMark(Course):
+    print("Printing Mark now")
+    for mark in Marks:
+        if mark.getCourse() == Course:
+            print([mark.getName(), mark.getMark()])
+
+
+inputMark(choseCourse())
+inputMark(choseCourse())
+printMark(choseCourse())
+printMark(choseCourse())
